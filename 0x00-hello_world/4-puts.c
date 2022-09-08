b025837b80aeefadc767835dc8968de78d4532f9
Adisa Tolani <adisatolani1@gmail.com>
2022-09-08 17:51:32 +0100

    Task 4: Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/*Print "Programming is like building a multilingual puzzle"
+ *
+ *Call the Header
+ *Define the main function
+ *Use put() for the required expression
+ *return 0;
+ */
+#include <stdio.h>
+int main(){
+    puts("Programming is like building a multilingual puzzle\n");
+    return 0;
+}
