3932ab6b0d990b19a52c804c22feeeabf55ebeee
Adisa Tolani <adisatolani1@gmail.com>
2022-09-10 04:57:47 +0100

    Task 4: When I was having that alphabet soup

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,5 @@
-
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 /**
  * main - Entry point
@@ -15,10 +12,8 @@ int main(void)
 	char low;
 
 	for (low = 'a'; low <= 'z'; low++)
-    {   
-        putchar(low);
-        putchar('\n');
-    }
-    
+		putchar(low);
+		putchar('\n');
+
 	return (0);
 }
