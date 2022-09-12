957c25c4b9ed40fd0b959ec91c8fe191fa417204
Adisa Tolani <adisatolani1@gmail.com>
2022-09-12 16:23:14 +0100

    Task 4: Print all lowercase letters without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - print all the lowercase letters except q and e
+ * Initialize variables
+ * Use the for loop
+ * Within the for loop, use the if condition
+ * Print the function
+ * return (0);
+ */
+
+
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		if (low != 'q' && low != 'e')
+		{
+			putchar(low);
+		}
+	}
+
+	return (0);
+}
