d8e77c340a4437980606eb6ea50500efa8abf2ce
Adisa Tolani <adisatolani1@gmail.com>
2022-09-12 17:05:54 +0100

    Task 6: print all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print all single digit numbers of base 10 starting from 0
+ * Declare variable
+ * Use the for loop
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num < '9'; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+
+	return (0);
+}
