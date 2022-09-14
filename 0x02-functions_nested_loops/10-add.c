246d700b851a3e810fb7c20b75aef0c76137620d
Adisa Tolani <adisatolani1@gmail.com>
2022-09-14 22:05:32 +0100

    Task 10: Adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - adds two integers and return the result
+ * @a: is one of the values passed in the function add
+ * @b: is one fo the values passed in the function add
+ *
+ * Return: 0 Always (success)
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = (a + b);
+
+	return (sum);
+}
