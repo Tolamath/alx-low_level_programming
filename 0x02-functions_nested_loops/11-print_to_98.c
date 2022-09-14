032b173c3a0c40fbca00d74280bd09e4ae448e6e
Adisa Tolani <adisatolani1@gmail.com>
2022-09-14 23:05:59 +0100

    Task 11: Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * print_to_98 - a function that prints all natural numbers from n to 98
+ * @n: is the argument passed to the function
+ *
+ * Return: 0 Always(success)
+ */
+void print_to_98(int n)
+{
+	int i = n, rem, div;
+	putchar(i + '0');
+	for (i = n + 1; i <= 98; i++)
+	{
+		putchar(',');
+		putchar(' ');
+
+		div = (i / 10);
+		rem = (i % 10);
+		if (i > 9)
+		{
+			putchar(div + '0');
+			putchar(rem + '0');
+		}
+		else if (i < 0)
+		{
+			printf("%d", i);
+		}
+		else
+		{
+			putchar(' ');
+			putchar(i + '0');
+		}
+	}
+	putchar('\n');
+}
+int main(void)
+{
+	print_to_98(-10);
+}
