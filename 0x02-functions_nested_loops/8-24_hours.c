3095d5fff7213e1589aa6f30e651aafd293cd37c
Adisa Tolani <adisatolani1@gmail.com>
2022-09-14 13:53:58 +0100

    Task: print every minutes of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,11 +19,11 @@ void jack_bauer(void)
 			min_remainder = (min % 10);
 			hours_remainder = (hours % 10);
 
-			_putchar(min / 10 + '0');
-			_putchar(min_remainder);
-			_putchar(':');
 			_putchar(hours / 10 + '0');
 			_putchar(hours_remainder);
+			_putchar(':');
+			_putchar(min / 10 + '0');
+			_putchar(min_remainder);
 			_putchar('\n');
 
 			min++;
