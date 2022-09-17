e227046c8c001f5a32203ea8f2bfc3075ff8f502
Adisa Tolani <adisatolani1@gmail.com>
2022-09-17 03:10:10 +0100

    Task 6: print lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,23 +2,18 @@
 /**
  * print_line - function that draws a straight line in the terminal
  * @n: is the parameter passed in the function
- *
- * Return: 0 Always(success)
  */
 void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
 		if (n <= 0)
 		{
-			_putchar('\n');
+			continue;
 		}
-		else
-		{
-			_putchar('_');
-		}
+		_putchar('_');
 	}
 	_putchar('\n');
 }
