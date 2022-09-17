91909e5d5c3791818fc23c39d4c5c04f8173be20
Adisa Tolani <adisatolani1@gmail.com>
2022-09-17 04:00:19 +0100

    Task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_square - a function that prints a square
+ * @size: is the parameter passed to the function
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i <= size; i++)
+		{
+			for (j = 0; j <= size; j++)
+			{
+				_putchar('#');
+			}
+		}
+	}
+	putchar('\n');
+}
