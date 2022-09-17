8460bf1859c1eebeae47bb74aedf7eea070b0f36
Adisa Tolani <adisatolani1@gmail.com>
2022-09-17 02:56:37 +0100

    Task 6: prints line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_line - function that draws a straight line in the terminal
+ * @n: is the parameter passed in the function
+ *
+ * Return: 0 Always(success)
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n <= 0)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
+
