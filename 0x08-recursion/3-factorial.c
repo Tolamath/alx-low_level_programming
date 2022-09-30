690f7759b5b6e355945b93fb41656a880649a9db
Adisa Tolani <adisatolani1@gmail.com>
2022-09-30 14:16:13 +0100

    Task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * factorial - prints the factorail of a function
+ * @n: The parameter passed to the factorial function
+ *
+ * Return: 0 Always(success)
+ */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
+
+/**
+ * main - prints the factorial of a number
+ *
+ *
+ * Return: 0 Always(success)
+ */
+int main(void)
+{
+	int m, n;
+
+	n = -1024;
+	m = factorial(n);
+	printf("The factorial of %d  is: %d\n", n, m);
+}
